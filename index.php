6eca33822fc5399119b2775326c2d80cdef447e7
Pierluigi Di Lorenzo <pierluigi.dilorenzo@gmail.com>
2020-06-08 13:08:12 +0200

    import message from postgresdb

---
+++ index.php
@@ -0,0 +1,55 @@
+<?php
+function main(array $args) : array {
+
+    require 'config.php';
+
+    if (! array_key_exists("dbname", $args) || ! array_key_exists("dbuser", $args) || ! array_key_exists("dbpass", $args)) {
+        $result = $emptyForm;
+        $json_result = json_decode($result);
+        return array("body" => $json_result);
+    }
+
+    // Default values for dbhost e dbport
+    if (! array_key_exists("dbhost", $args)) {
+        $args['dbhost'] = "localhost";
+    } 
+    if (! array_key_exists("dbport", $args)) {
+        $args['dbport'] = "5432";
+    }
+
+    $dsn = sprintf("pgsql:host=%s port=%d dbname=%s user=%s password=%s",
+                      $args['dbhost'],
+                      $args['dbport'],
+                      $args['dbname'],
+                      $args['dbuser'],
+                      $args['dbpass']);
+  
+    try {
+        $connection = new PDO($dsn);
+        $sth = $connection->prepare("SELECT 0 AS amount,
+                                        scadenza AS due_date,
+                                        destinatario AS fiscal_code,
+                                        false AS invalid_after_due_date,
+                                        testo AS markdown,
+                                        1 AS notice_number,
+                                        titolo AS subject FROM messages");
+        $sth->execute();
+        
+    } catch (PDOException $e) {
+            print($e->getMessage() . "\n");
+            return array("body" => $e->getMessage());
+    }
+      
+    //Fetch all rows in the result set
+    $result['data'] = $sth->fetchAll(PDO::FETCH_ASSOC);
+
+    //JSON encode
+    $json_result = json_encode($result);
+    print_r($json_result);
+
+    return array(
+    "body" => $json_result
+    );
+}
+?>
+
